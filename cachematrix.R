986b014034cac4bbf859aa015b45fbd52d03dc8d
mvssrmurthy <mvssrmurthy@gmail.com>
2017-04-03 23:14:39 -0700

    Implemented Cached solution to give inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The solution has two functions one constructs a special matrix and other
+## accepts the special matrix and returns the inverse of the matrix.
+## It improves the performance of the invers of a matrix by caching the result.
 
-## Write a short comment describing this function
+## This function constructs a special matrix and returns the list of access methods to this special matrix
+## It has setter and getter methods of the matrix and inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    iMatrix <- NULL
+    set <- function(y){
+        x <<- y
+        iMatrix <<- NULL
+    }
+    get <- function() x
+    setinversematrix <- function(inverseMatrix) iMatrix <<- inverseMatrix
+    getinversematrix <- function() iMatrix
+    list(set = set, get = get, setinversematrix = setinversematrix, 
+         getinversematrix = getinversematrix)
 }
 
 
-## Write a short comment describing this function
+## This method returns the inverse of a give special matrix. It uses solve method.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    iMatrix <- x$getinversematrix()
+    if(!is.null(iMatrix)) {
+        message("getting cached data")
+        return(iMatrix)
+    }
+    dataMatrix <- x$get()
+    iMatrix <- solve(dataMatrix,...)
+    x$setinversematrix(iMatrix)
+    iMatrix
 }
